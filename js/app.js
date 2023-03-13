ca2e8bcdaf3c94fd0927c4a27d111565c15a2821
MatthewChong <chiongmatthew96@gmail.com>
2023-03-13 13:09:08 +0800

    animation

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const aboutBtn = document.querySelector(".about-btn");
+const portfolioBtn = document.querySelector(".portfolio-btn");
+const about = document.querySelector(".about");
+const portfolio = document.querySelector(".portfolio");
+
+aboutBtn.addEventListener("click", (e) => {
+  const currentBtn = document.querySelector(".current-btn");
+  const currentSec = document.querySelector(".current-sec");
+
+  currentBtn.classList.remove("current-btn");
+  about.classList.remove("current-btn");
+  aboutBtn.classList.add("current-btn");
+  currentSec.classList.remove("current-sec");
+  about.classList.add("current-sec");
+});
+portfolioBtn.addEventListener("click", (e) => {
+  const currentBtn = document.querySelector(".current-btn");
+  const currentSec = document.querySelector(".current-sec");
+
+  currentBtn.classList.remove("current-btn");
+  portfolioBtn.classList.add("current-btn");
+  currentSec.classList.remove("current-sec");
+  portfolio.classList.add("current-sec");
+});
